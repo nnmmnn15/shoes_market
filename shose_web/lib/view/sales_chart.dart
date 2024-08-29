364702772f8abde662596de726c38c53a7d4a028
Leejongnam <leejong16@naver.com>
2024-08-29 11:22:25 +0900

    jongnam

---
+++ shose_web/lib/view/sales_chart.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SalesChart extends StatefulWidget {
+  const SalesChart({super.key});
+
+  @override
+  State<SalesChart> createState() => _SalesChartState();
+}
+
+class _SalesChartState extends State<SalesChart> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
